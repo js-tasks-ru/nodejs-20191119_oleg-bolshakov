6be3f3071bba775f68eb74d3bb7c165aadbdc875
unknown <oleg@bolshakov.net>
2019-11-21 21:03:01 +0300

    add solution for 0-module 1-task

--- 0-module/1-task/sum.js
+++ 0-module/1-task/sum.js
@@ -1,5 +1,13 @@
 function sum(a, b) {
   /* ваш код */
+  function isNumber (value) {
+    if ( typeof value !== 'number' || !isFinite(value) ) {
+      throw new TypeError(`Value: "${value}" is not a number.`);
+    };
+  }
+  isNumber(a);
+  isNumber(b);
+  return a + b;
 }
 
 module.exports = sum;
